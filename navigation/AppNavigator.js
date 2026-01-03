726cef6eaea0a89d575d08ed4069de1acd895d5f
Nasirah Khan <nasirahkhan01@gmail.com>
2026-01-03 20:36:54 +0500

    Update

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -1,33 +1,3 @@
-// import React, { useContext } from "react";
-// import { NavigationContainer } from "@react-navigation/native";
-// import { createNativeStackNavigator } from "@react-navigation/native-stack";
-// import { AuthContext } from "../context/AuthContext";
-// import LoginScreen from "../screens/LoginScreen";
-// import ProductListScreen from "../screens/ProductListScreen";
-// import ProductDetailScreen from "../screens/ProductDetailScreen";
-
-// const Stack = createNativeStackNavigator();
-
-// export default function AppNavigator() {
-//   const { token } = useContext(AuthContext);
-
-//   return (
-//     <NavigationContainer>
-//       <Stack.Navigator>
-//         {!token ? (
-//           <Stack.Screen name="Login" component={LoginScreen} />
-//         ) : (
-//           <>
-//             <Stack.Screen name="Products" component={ProductListScreen} />
-//             <Stack.Screen name="Details" component={ProductDetailScreen} />
-//           </>
-//         )}
-//       </Stack.Navigator>
-//     </NavigationContainer>
-//   );
-// }
-
-
 
 import React, { useContext } from "react";
 import { NavigationContainer } from "@react-navigation/native";
